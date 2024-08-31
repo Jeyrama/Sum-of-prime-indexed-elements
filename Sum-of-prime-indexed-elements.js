80935950aa88c37bf32867c21b31de7a6cf0506f
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-08-31 09:37:46 -0700

    Update Sum-of-prime-indexed-elements.js

--- Sum-of-prime-indexed-elements.js
+++ Sum-of-prime-indexed-elements.js
@@ -6,3 +6,23 @@ The first element of the array is at index 0.
 
 
 // Solution
+
+function total(arr) {
+  let sumPrimeIndices = 0;
+  for (let i = 2; i < arr.length; i++) {
+    if (isPrime(i)) {
+      sumPrimeIndices += arr[i];
+    }
+  }
+  return sumPrimeIndices;
+}
+
+function isPrime(num) {
+  let root = Math.sqrt(num)
+  for (let i = 2; i <= root; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
